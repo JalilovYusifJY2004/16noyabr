5d4c4d743157ef3219f3aac0079ca2e089edf40b
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-11-15 19:40:40 +0400

    add

---
+++ Program.cs
@@ -0,0 +1,22 @@
+namespace _16noyabr
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllerRoute(
+                "default",
+                "{controller=home}/{action=index}/{id?}"
+
+                );
+
+            app.Run();
+        }
+    }
+}
